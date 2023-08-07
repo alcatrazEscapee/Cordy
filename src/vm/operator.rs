2f7f3aaaaf69df3f0e38faa4dfe1e1b8d39d69a8
alcatrazEscapee <35673674+alcatrazEscapee@users.noreply.github.com>
2023-08-06 19:55:55 -0600

    Add missing handling for `is heap`

--- src/vm/operator.rs
+++ src/vm/operator.rs
@@ -208,6 +208,7 @@ pub fn binary_is(lhs: ValuePtr, rhs: ValuePtr, invert: bool) -> ValueResult {
             NativeFunction::Dict => lhs.is_dict(),
             NativeFunction::Vector => lhs.is_vector(),
             NativeFunction::Iterable => lhs.is_iter(),
+            NativeFunction::Heap => lhs.is_heap(),
             NativeFunction::Any => true,
             _ => return TypeErrorBinaryIs(lhs, rhs).err()
         },
