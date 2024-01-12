1cef6bd1790b7e92d27303c8760f4af836ce56a4
AlcatrazEscapee <alex@molleroneill.com>
2024-01-12 16:56:03 -0500

    Fix another invalid `OpcodeId` crash.

--- cordy-sys/src/compiler/parser/mod.rs
+++ cordy-sys/src/compiler/parser/mod.rs
@@ -2388,7 +2388,8 @@ mod tests {
     #[test] fn test_weird_closure_not_a_closure() { run!("weird_closure_not_a_closure"); }
     #[test] fn test_weird_locals() { run!("weird_locals"); }
     #[test] fn test_weird_loop_nesting_in_functions() { run!("weird_loop_nesting_in_functions"); }
-    #[test] fn test_weird_opcode_id() { run!("weird_opcode_id"); }
+    #[test] fn test_weird_opcode_id_1() { run!("weird_opcode_id_1"); }
+    #[test] fn test_weird_opcode_id_2() { run!("weird_opcode_id_2"); }
     #[test] fn test_weird_structs() { run!("weird_structs"); }
     #[test] fn test_weird_upvalue_index() { run!("weird_upvalue_index"); }
     #[test] fn test_weird_upvalue_index_with_parameter() { run!("weird_upvalue_index_with_parameter"); }
